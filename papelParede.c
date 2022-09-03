21a28e851e71afedd0bd1125f68d56f7053d03c5
Leo Lomardo <leoland771@gmail.com>
2022-09-03 20:38:34 -0300

    papelParede

--- papelParede.c
+++ papelParede.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_LINK 200
 
-int main()
-{
+int main(){
+     char command[] = "wget --output-document=saida.jpg -nd -r -P /media/HD/DOCUMENTOS/Github/testa-tudo -A jpeg,jpg,bmp,gif,png ";  
+     char link[MAX_LINK];
 
-     char command[] = "wget -A png,jpeg,jpg,gif -r https://en.wikipedia.org/wiki/Botafogo_de_Futebol_e_Regatas_%28basketball%29";  
+     printf("Insira o link da imagem que deseja inserir:");
+     scanf("%s", link);
+
+     strcat(command,link);
+
      system(command); 
+     system("gsettings set org.gnome.desktop.background picture-uri file://///media/HD/DOCUMENTOS/Github/testa-tudo/saida.jpg");
      return 0;
 }
