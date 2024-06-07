134c74eb6554711785561d1c7ea4bfd4108461e9
Leo Lomardo <leoland771@gmail.com>
2024-06-06 23:34:49 -0300

    vamo q vamo

--- CRC-CALCULADORA/calcCRC.c
+++ CRC-CALCULADORA/calcCRC.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <locale.h>
 
-// Função para calcular o CRC
+// FunÃ§Ã£o para calcular o CRC
 void calculateCRC(char *message, char *generator, FILE *arquivo, char *resultado) {
     int msgLen = strlen(message);
     int genLen = strlen(generator);
@@ -24,8 +24,7 @@ void calculateCRC(char *message, char *generator, FILE *arquivo, char *resultado
     for (int i = 0; i < msgLen; i++) {
         if (dividend[i] == '1') {
             quotient[i] = '1';
-            fprintf(arquivo, "\\underline{Passo %d:}\n\n", step);
-            fprintf(arquivo, "\\begin{verbatim}\n");
+            fprintf(arquivo, "Passo %d:\n", step);
             fprintf(arquivo, "%s <--- entrada atualizada\n", dividend);
             fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator);
             fprintf(arquivo, "-------------------------------------\n");
@@ -34,97 +33,105 @@ void calculateCRC(char *message, char *generator, FILE *arquivo, char *resultado
                 dividend[i + k] = (dividend[i + k] == generator[k]) ? '0' : '1';
             }
             fprintf(arquivo, "%s <--- resultado\n", dividend);
-            fprintf(arquivo, "\\end{verbatim}\n\n");
             step++;
         } else if (dividend[i] == '0') {
-            fprintf(arquivo, "\\underline{Passo %d:}\n\n", step);
-            fprintf(arquivo, "\\begin{verbatim}\n");
+            fprintf(arquivo, "Passo %d:\n", step);
             fprintf(arquivo, "%s <--- entrada atualizada\n", dividend);
             fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator);
             fprintf(arquivo, "-------------------------------------\n");
-            fprintf(arquivo, "%s <--- resultado - sem alterações\n", dividend);
-            fprintf(arquivo, "\\end{verbatim}\n\n");
+            fprintf(arquivo, "%s <--- resultado - sem alteraÃ§Ãµes\n", dividend);
             step++;
         } else {
-            printf("Caractere inválido\n");
+            printf("Caractere invÃ¡lido\n");
             exit(1);
         }
     }
-    fprintf(arquivo, "\\textbf{Resto: }\n\n");
-    fprintf(arquivo, "\\begin{verbatim}\n");
-    fprintf(arquivo, "%s <--- resultado após %d passos de execução\n", dividend, step);
-    fprintf(arquivo, "\\end{verbatim}\n\n");
-
+    
     fprintf(arquivo, "Resto: %s\n", dividend + msgLen);
     printf("Resto: %s\n", dividend + msgLen);
     strcpy(resultado, dividend + msgLen);
     fprintf(arquivo, "Quociente: %s\n", quotient);
     printf("Quociente: %s\n", quotient);
+    fprintf(arquivo, "\nNumero de Passos:%d\n",step - 1);
 }
 
-// Função para evoluir o LFSR ao percorrer a mensagem e gerar o conteúdo LaTeX
-void lfsrWithMessage(char *initialState, char *message, FILE *arquivo) {
-    int stateLen = strlen(initialState);
+void calculateLFSR(char *message, char *generator, FILE *arquivo) {
     int msgLen = strlen(message);
-    char state[stateLen + 1];
-    strcpy(state, initialState);
+    int genLen = strlen(generator);
 
-    fprintf(arquivo, "Estado inicial do LFSR: %s\n", state);
-    printf("Estado inicial do LFSR: %s\n", state);
+    int lfsr[genLen - 1];
+    for (int i = 0; i < genLen - 1; i++) {
+        lfsr[i] = 0;
+    }
+    fprintf(arquivo, "\\begin{table}[h!]\n\\centering\n\\begin{tabular}{|c|c|c|c|c|c|c|c|l|}\n");
+    fprintf(arquivo, "\\hline\n");
+    fprintf(arquivo, "Passo & R5 & R4 & R3 & R2 & R1 & R0 & Feedback & Bits Restantes \\\\ \\hline\n");
 
-    // LaTeX table header
-    fprintf(arquivo, "\\begin{table}[h!]\n");
-    fprintf(arquivo, "\\centering\n");
-    fprintf(arquivo, "\\begin{tabular}{|c|c|c|c|c|c|c|c|c|c|}\n");
-    fprintf(arquivo, "\\hline\n");
-    fprintf(arquivo, "$t$ & $R_6$ & $R_5$ & $R_4$ & $R_3$ & $R_2$ & $R_1$ & $R_0$ & $I(t)$ & $O(t)$ \\\\\n");
-    fprintf(arquivo, "\\hline\n");
+    fprintf(arquivo, "%4d & ", 0);
+    for (int j = genLen - 2; j >= 0; j--) {
+        fprintf(arquivo, "0 & ");
+    }
+    fprintf(arquivo, "0 &  ");
+    for (int j = 0; j < msgLen; j++) {
+        fprintf(arquivo, "%c", message[j]);
+    }
+    fprintf(arquivo, " \\\\\n\\hline\n");
 
     for (int i = 0; i < msgLen; i++) {
-        // Bit de saída é o bit mais à direita do registro
-        char bitSaida = state[stateLen - 1];
-
-        // Calcular o novo bit de feedback
-        char feedback = (state[stateLen - 1] - '0') ^ (state[stateLen - 2] - '0') ^ (state[stateLen - 4] - '0') ^ (message[i] - '0');
-        feedback = feedback ? '1' : '0'; // Assegurar que o feedback seja '1' ou '0'
-
-        // Deslocar os bits do registro para a direita
-        for (int j = stateLen - 1; j > 0; j--) {
-            state[j] = state[j - 1];
+        int feedback = lfsr[genLen - 2] ^ (message[i] - '0');
+        for (int j = genLen - 2; j > 0; j--) {
+            lfsr[j] = lfsr[j - 1] ^ (feedback & (generator[j] - '0'));
         }
-        state[0] = feedback; // Inserir o novo bit de feedback na posição mais à esquerda
-
-        // Escrever estado atual na tabela LaTeX
-        fprintf(arquivo, "%d & ", i + 1);
-        for (int j = 0; j < stateLen; j++) {
-            fprintf(arquivo, "%c & ", state[j]);
+        lfsr[0] = feedback;
+        
+        fprintf(arquivo, "%4d & ", i + 1);
+        for (int j = genLen - 2; j >= 0; j--) {
+            fprintf(arquivo, "%d & ", lfsr[j]);
         }
-        fprintf(arquivo, "%c & %c \\\\\n", message[i], bitSaida);  // Incluir I(t) e O(t)
-        fprintf(arquivo, "\\hline\n");
-
-        printf("Passo %d: %s, I(t): %c, O(t): %c\n", i + 1, state, message[i], bitSaida);
+        fprintf(arquivo, "%d &  ", feedback);
+        if (i == (msgLen - 1)) {
+            fprintf(arquivo, "-");
+        } else {
+            for (int j = i + 1; j < msgLen; j++) {
+                fprintf(arquivo, "%c", message[j]);
+            }
+        }
+        fprintf(arquivo, " \\\\\n\\hline\n");
     }
 
-    // LaTeX table footer
-    fprintf(arquivo, "\\end{tabular}\n");
-    fprintf(arquivo, "\\caption{Evolução do LFSR}\n");
-    fprintf(arquivo, "\\label{tab:lfsr}\n");
-    fprintf(arquivo, "\\end{table}\n");
+    for (int i = 0; i < genLen - 1; i++) {
+        int feedback = lfsr[genLen - 2]; 
+        for (int j = genLen - 2; j > 0; j--) {
+            lfsr[j] = lfsr[j - 1] ^ (feedback & (generator[j] - '0'));
+        }
+        lfsr[0] = feedback;
+
+        fprintf(arquivo, "%4d & ", msgLen + i + 1);
+        for (int j = genLen - 2; j >= 0; j--) {
+            fprintf(arquivo, "%d & ", lfsr[j]);
+        }
+        fprintf(arquivo, "%d & - \\\\\n\\hline\n", feedback);
+    }
+
+    fprintf(arquivo, "\\end{tabular}\n\\caption{EvoluÃ§Ã£o do LFSR}\n\\end{table}\n");
 }
+
+
+
 
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
     // Mensagem de entrada de 32 bits
     char message[] = "11001110110110101010101110110101";
-
-    // Polinômio gerador: X^6 + X^5 + X^3 + X + 1
+    char messageAumentada[] = "110011101101101010101011101101010000000";
+    // PolinÃ´mio gerador: X^6 + X^5 + X^3 + X + 1
     char generator[] = "1101011";
     int lenGen = strlen(generator);
     int lenMsg = strlen(message);
 
     char resultado[lenGen + 1];
-    char resultadoFinal[lenGen + lenMsg]; // Inicializar com string vazia
+    char resultadoFinal[lenGen + lenMsg];
 
     FILE *arquivoCRC = fopen("crc.txt", "w");
     if (arquivoCRC == NULL) {
@@ -132,9 +139,9 @@ int main() {
         exit(1);
     }
     fprintf(arquivoCRC, "Mensagem original: %s\n", message);
-    fprintf(arquivoCRC, "Polinômio gerador: %s\n", generator);
+    fprintf(arquivoCRC, "PolinÃ´mio gerador: %s\n", generator);
     printf("Mensagem original: %s\n", message);
-    printf("Polinômio gerador: %s\n", generator);
+    printf("PolinÃ´mio gerador: %s\n", generator);
 
     calculateCRC(message, generator, arquivoCRC, resultado);
 
@@ -144,16 +151,11 @@ int main() {
     fprintf(arquivoCRC, "Resultado final: %s\n", resultadoFinal);
     fclose(arquivoCRC);
 
-    // LFSR
-    char initialState[7] = "000000";  // Definir estado inicial
-    FILE *arquivoLFSR = fopen("lfsr.txt", "w");
-    if (arquivoLFSR == NULL) {
-        perror("Erro na abertura do arquivo!");
-        exit(1);
-    }
+    FILE *arquivoLFSRmsgnNormal = fopen("lfsrMsgnNormal.tex", "w");
 
-    lfsrWithMessage(initialState, message, arquivoLFSR); // Evoluir o LFSR percorrendo a mensagem
-    fclose(arquivoLFSR);
-
+    calculateLFSR(message, generator, arquivoLFSRmsgnNormal);
+    
+    fclose(arquivoLFSRmsgnNormal);
     return 0;
 }
+
--- CRC-CALCULADORA/crc2.c
+++ CRC-CALCULADORA/crc2.c
@@ -1,94 +1,159 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
 
+// Função para calcular o CRC
 void calculateCRC(char *message, char *generator, FILE *arquivo, char *resultado) {
     int msgLen = strlen(message);
     int genLen = strlen(generator);
-    char dividend[msgLen + genLen]; // Alocar espaço suficiente para a mensagem aumentada
-    char quotient[msgLen + 1]; // Quociente terá o mesmo tamanho da mensagem original
+
+    char dividend[msgLen + genLen];
+    char quotient[msgLen + 1];
 
     strcpy(dividend, message);
     for (int i = 0; i < genLen - 1; i++) {
         strcat(dividend, "0");
     }
-    memset(quotient, '0', msgLen); // Inicializa o quociente com '0's
-    quotient[msgLen] = '\0'; // Termina a string do quociente
-
+    memset(quotient, '0', msgLen);
+    quotient[msgLen] = '\0';
     fprintf(arquivo, "Mensagem aumentada: %s\n\n", dividend);
     printf("Mensagem aumentada: %s\n", dividend);
+
     int step = 1;
-    
-    for (int i = 0; i <= msgLen; i++) {
+    for (int i = 0; i < msgLen; i++) {
         if (dividend[i] == '1') {
-            quotient[i] = '1'; // Define o bit do quociente como '1'
-            
-            // Imprimir o estado atual do dividendo
-            fprintf(arquivo, "Passo %d:\n", step);
-            fprintf(arquivo, "%s <--- entrada deslocada para a direita com %d zeros\n", dividend, i);
-            fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator); // Alinha o divisor com a parte do dividendo
-            fprintf(arquivo, "------------------\n");
+            quotient[i] = '1';
+            fprintf(arquivo, "\\underline{Passo %d:}\n\n", step);
+            fprintf(arquivo, "\\begin{verbatim}\n");
+            fprintf(arquivo, "%s <--- entrada atualizada\n", dividend);
+            fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator);
+            fprintf(arquivo, "-------------------------------------\n");
 
             for (int k = 0; k < genLen; k++) {
                 dividend[i + k] = (dividend[i + k] == generator[k]) ? '0' : '1';
             }
-
-            // Imprimir o resultado da operação XOR
-            fprintf(arquivo, "%s <--- resultado\n\n", dividend);
-
+            fprintf(arquivo, "%s <--- resultado\n", dividend);
+            fprintf(arquivo, "\\end{verbatim}\n\n");
             step++;
-        }else if( dividend[i] == 0){
-            fprintf(arquivo, "Passo %d:\n", step);
-            fprintf(arquivo, "%s <--- entrada deslocada para a direita com %d zeros\n", dividend, i);
-            fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator); // Alinha o divisor com a parte do dividendo
-            fprintf(arquivo, "------------------\n");
+        } else if (dividend[i] == '0') {
+            fprintf(arquivo, "\\underline{Passo %d:}\n\n", step);
+            fprintf(arquivo, "\\begin{verbatim}\n");
+            fprintf(arquivo, "%s <--- entrada atualizada\n", dividend);
+            fprintf(arquivo, "%*s%s <--- divisor\n", i, "", generator);
+            fprintf(arquivo, "-------------------------------------\n");
+            fprintf(arquivo, "%s <--- resultado - sem alterações\n", dividend);
+            fprintf(arquivo, "\\end{verbatim}\n\n");
             step++;
+        } else {
+            printf("Caractere inválido\n");
+            exit(1);
         }
     }
+    fprintf(arquivo, "\\textbf{Resto: }\n\n");
+    fprintf(arquivo, "\\begin{verbatim}\n");
+    fprintf(arquivo, "%s <--- resultado após %d passos de execução\n", dividend, step);
+    fprintf(arquivo, "\\end{verbatim}\n\n");
 
-    // Copiar o resto final para a variável resultado
-    fprintf(arquivo, "Resto final: %s\n", dividend + msgLen);
-    printf("Resto final: %s\n", dividend + msgLen);
+    fprintf(arquivo, "Resto: %s\n", dividend + msgLen);
+    printf("Resto: %s\n", dividend + msgLen);
     strcpy(resultado, dividend + msgLen);
-
-    // Imprimir o quociente
     fprintf(arquivo, "Quociente: %s\n", quotient);
     printf("Quociente: %s\n", quotient);
 }
 
-int main() {
-    // Mensagem de entrada de 32 bits
-    char message[] = "101011"; 
-
-    // Polinômio gerador: X^6 + X^5 + X^3 + X + 1
-    char generator[] = "1101";
+void calculateLFSR(char *message, char *generator, FILE *arquivo) {
     int msgLen = strlen(message);
     int genLen = strlen(generator);
-    char resultado[genLen+1];
-    char resultadoFinal[genLen+msgLen+1];  // Inicializar com string vazia
 
-    // Limpar o conteúdo do arquivo antes de iniciar o cálculo
-    FILE *arquivo = fopen("crc2.txt", "w+");
-    if (arquivo == NULL) {
+    // Inicializa o LFSR com todos os flip-flops em 0
+    int lfsr[genLen - 1];
+    for (int i = 0; i < genLen - 1; i++) {
+        lfsr[i] = 0;
+    }
+
+    // Imprime o cabeçalho da tabela
+    fprintf(arquivo, "Passo\tI\t");
+    for (int i = genLen - 1; i >= 0; i--) {
+        fprintf(arquivo, "R%d\t", i);
+    }
+    fprintf(arquivo, "\n");
+
+    // Processa cada bit da mensagem
+    for (int i = 0; i < msgLen; i++) {
+        int input_bit = message[i] - '0';
+        int feedback = lfsr[genLen - 2] ^ input_bit;
+
+        fprintf(arquivo, "Passo %d\t%d\t", i + 1, input_bit);
+
+        // Atualiza os estados do LFSR
+        for (int j = genLen - 2; j > 0; j--) {
+            lfsr[j] = lfsr[j - 1];
+        }
+        lfsr[0] = feedback;
+
+        // Realiza o XOR com o polinômio gerador
+        for (int j = 0; j < genLen - 1; j++) {
+            if (generator[j] == '1') {
+                lfsr[j] ^= feedback;
+            }
+        }
+
+        // Imprime o estado atual do LFSR
+        for (int j = genLen - 2; j >= 0; j--) {
+            fprintf(arquivo, "%d\t", lfsr[j]);
+        }
+        fprintf(arquivo, "\n");
+    }
+
+    // Imprime o estado final
+    fprintf(arquivo, "Final\t");
+    for (int i = genLen - 2; i >= 0; i--) {
+        fprintf(arquivo, "%d\t", lfsr[i]);
+    }
+    fprintf(arquivo, "\n");
+}
+int main() {
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    // Mensagem de entrada de 32 bits
+    char message[] = "11001110110110101010101110110101";
+
+    // Polinômio gerador: X^6 + X^5 + X^3 + X + 1
+    char generator[] = "1101011";
+    int lenGen = strlen(generator);
+    int lenMsg = strlen(message);
+
+    char resultado[lenGen + 1];
+    char resultadoFinal[lenGen + lenMsg];
+
+    FILE *arquivoCRC = fopen("crc.txt", "w");
+    if (arquivoCRC == NULL) {
         perror("Erro na abertura do arquivo!");
         exit(1);
     }
-
-    
-    // Reabrir o arquivo no modo de adição
-
-
-    fprintf(arquivo, "Mensagem original: %s\n", message);
-    fprintf(arquivo, "Polinomio gerador: %s\n", generator);
+    fprintf(arquivoCRC, "Mensagem original: %s\n", message);
+    fprintf(arquivoCRC, "Polinômio gerador: %s\n", generator);
     printf("Mensagem original: %s\n", message);
-    printf("Polinomio gerador: %s\n", generator);
-    
-    calculateCRC(message, generator, arquivo, resultado);
+    printf("Polinômio gerador: %s\n", generator);
+
+    calculateCRC(message, generator, arquivoCRC, resultado);
 
     strcat(resultadoFinal, message);
     strcat(resultadoFinal, resultado);
     printf("Resultado final: %s\n", resultadoFinal);
-    fprintf(arquivo,"Resultado final: %s\n", resultadoFinal);
-    fclose(arquivo);
+    fprintf(arquivoCRC, "Resultado final: %s\n", resultadoFinal);
+    fclose(arquivoCRC);
+
+    FILE *arquivoLFSR = fopen("lfsr2.txt", "w");
+    FILE *arquivoLFRSmsnAumentada = fopen("lfsrMsgAumentada.txt", "w");
+    FILE *arquivoLFSRres = fopen("lfsrRes.txt", "w");   
+    if (arquivoLFSR == NULL) {
+        perror("Erro na abertura do arquivo!");
+        exit(1);
+    }
+    calculateLFSR(message, generator, arquivoLFSR);
+    fclose(arquivoLFSR);
+
     return 0;
 }
